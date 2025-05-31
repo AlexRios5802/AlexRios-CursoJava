dc56ae5de279bb7b5402d2670d0dd3b44ec3ad9b
Alex Rios <al066668@uacam.mx>
2025-05-30 22:30:14 -0600

    Código de arreglos, es código básico, no tiene cosas de gran relevancia

---
+++ Arreglos/src/Arreglos.java
@@ -0,0 +1,10 @@
+public class Arreglos {
+    public static void main(String[] args) {
+        // Arreglos
+        // 1. Declaramos un arreglo
+        int[] enteros;
+        // 2. Inicializar el arreglo
+        enteros = new int[3];
+
+    }
+}
