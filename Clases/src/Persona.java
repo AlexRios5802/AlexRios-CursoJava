3049ef3c4bf1486ac86b19524b2950426ecec2ed
Alex Rios <al066668@uacam.mx>
2025-07-10 00:11:20 -0600

    Ahora creamos objetos tipo perosona en Java

--- Clases/src/Persona.java
+++ Clases/src/Persona.java
@@ -3,9 +3,23 @@ public class Persona { // Nombre de la clase: Persona - Características
     String nombre; // Todos sus valores por Default es NULL. NO PODEMOS USAR VAR, ya que requiere de un valor. y para este bloque no puede tener valores
     String apellido;
 
-
     void mostrarPersona(){ // Métodos
         System.out.println("Nombre: " + nombre); // Notamos que el color de la variable es diferente a un variable local
         System.out.println("Apellido: " + apellido);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("*** Creación de Clase y Objetos Persona ***");
+        var objeto1 = new Persona(); // Solo reserva espacio en la memoria, pero no agrega valores
+        objeto1.nombre = "Alex";
+        objeto1.apellido = "Rios";
+        objeto1.mostrarPersona();
+
+        // Segundo objeto
+        System.out.println();
+        var objeto2 = new Persona();
+        objeto2.nombre = "Omar";
+        objeto2.apellido = "Perez";
+        objeto2.mostrarPersona();
     }
 }
