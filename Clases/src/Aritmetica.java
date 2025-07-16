ea91c41bbc06a2d0fa631044f65f49a636a4c327
Alex Rios <al066668@uacam.mx>
2025-07-15 23:39:50 -0600

    Agregamos un Scanner para que el usuario ongrese suspropios datos, danto así un estílo más dinámico

--- Clases/src/Aritmetica.java
+++ Clases/src/Aritmetica.java
@@ -1,33 +1,41 @@
+import java.util.Scanner;
+
 public class Aritmetica {
     int operando1;
     int operando2;
 
-    void sumar (){
+    void sumar() {
         var resultado = operando1 + operando2;
         System.out.println("Resultado de la suma: " + resultado);
     }
 
-    void resta (){
+    void resta() {
         var resultado2 = operando1 - operando2;
         System.out.println("Resultado de la resta: " + resultado2);
+    }
+
+    void input() {
+        Scanner consola = new Scanner(System.in);
+        System.out.print("Ingresa el valor para el operando1: ");
+        operando1 = consola.nextInt();
+        System.out.print("Ingresa el valor para el operando1: ");
+        operando2 = consola.nextInt();
     }
 
     public static void main(String[] args) {
         System.out.println("*** Ejemplo Aritmetica ***");
         var aritmetica1 = new Aritmetica();
-        aritmetica1.operando1 = 5;
-        aritmetica1.operando2 = 7;
+        aritmetica1.input();
         aritmetica1.sumar();
         aritmetica1.resta();
 
         System.out.println();
 
         var aritmetica2 = new Aritmetica();
-        aritmetica2.operando1 = 12;
-        aritmetica2.operando2 = 16;
+        aritmetica2.input(); // -> Se parece a un ciclo, ya que te pide de nuevo valores, pero no lo es, ya que está llamando el método "input"
         aritmetica2.sumar();
         aritmetica2.resta();
-        
+
 
     }
 
