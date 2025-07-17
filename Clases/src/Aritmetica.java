e77db70705ae018c9776bb65effffd7302198ff6
Alex Rios <al066668@uacam.mx>
2025-07-16 23:18:11 -0600

    Agregamos un constructor, para ve como funciona, y como se ve, sabiendo que este se agrega automático, decidimos agregar uno manualmente para ver como se construye

--- Clases/src/Aritmetica.java
+++ Clases/src/Aritmetica.java
@@ -4,6 +4,12 @@ public class Aritmetica {
     int operando1;
     int operando2;
 
+    public Aritmetica(int op1, int op2){
+        System.out.println("Ejecutando constructor");
+        operando1 = op1;
+        operando2 = op2;
+    }
+
     void sumar() {
         var resultado = operando1 + operando2;
         System.out.println("Resultado de la suma: " + resultado);
@@ -14,27 +20,27 @@ public class Aritmetica {
         System.out.println("Resultado de la resta: " + resultado2);
     }
 
-    void input() {
-        Scanner consola = new Scanner(System.in);
-        System.out.print("Ingresa el valor para el operando1: ");
-        operando1 = consola.nextInt();
-        System.out.print("Ingresa el valor para el operando1: ");
-        operando2 = consola.nextInt();
-    }
+    // void input() {
+    //     Scanner consola = new Scanner(System.in);
+    //     System.out.print("Ingresa el valor para el operando1: ");
+    //     operando1 = consola.nextInt();
+    //     System.out.print("Ingresa el valor para el operando1: ");
+    //     operando2 = consola.nextInt();
+    // }
 
     public static void main(String[] args) {
         System.out.println("*** Ejemplo Aritmetica ***");
-        var aritmetica1 = new Aritmetica();
-        aritmetica1.input();
+        var aritmetica1 = new Aritmetica(5, 7);
+        // aritmetica1.input();
         aritmetica1.sumar();
         aritmetica1.resta();
 
-        System.out.println();
+        // System.out.println();
 
-        var aritmetica2 = new Aritmetica();
-        aritmetica2.input(); // -> Se parece a un ciclo, ya que te pide de nuevo valores, pero no lo es, ya que está llamando el método "input"
-        aritmetica2.sumar();
-        aritmetica2.resta();
+        // var aritmetica2 = new Aritmetica();
+        // aritmetica2.input(); // -> Se parece a un ciclo, ya que te pide de nuevo valores, pero no lo es, ya que está llamando el método "input"
+        // aritmetica2.sumar();
+        // aritmetica2.resta();
 
 
     }
