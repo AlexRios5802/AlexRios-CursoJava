1fab368949ee128cb50714082aa213d3927f05ad
Alex Rios <al066668@uacam.mx>
2025-07-17 22:23:53 -0600

    Separamos las versiones del código del tema de Aritmética para que sea más fácil su lectura

--- Clases/src/AritmeticaVariablesDeclaradas.java
+++ Clases/src/AritmeticaVariablesDeclaradas.java
@@ -1,43 +1,30 @@
-import java.util.Scanner;
-
-public class Aritmetica {
+public class AritmeticaVariablesDeclaradas {
     int operando1;
     int operando2;
 
-    // Constructor vacío
-    public Aritmetica(){
-    }
-
-    // Constructor con dos argumentos
-    public Aritmetica(int op1, int op2){
-        System.out.println("Ejecutando constructor");
-        operando1 = op1;
-        operando2 = op2;
-    }
-
-    void sumar() {
+    void sumar (){
         var resultado = operando1 + operando2;
         System.out.println("Resultado de la suma: " + resultado);
     }
 
-    void resta() {
+    void resta (){
         var resultado2 = operando1 - operando2;
         System.out.println("Resultado de la resta: " + resultado2);
     }
 
-
     public static void main(String[] args) {
         System.out.println("*** Ejemplo Aritmetica ***");
-        var aritmetica1 = new Aritmetica(5, 7);
-        // aritmetica1.input();
+        var aritmetica1 = new Aritmetica();
+        aritmetica1.operando1 = 5;
+        aritmetica1.operando2 = 7;
         aritmetica1.sumar();
         aritmetica1.resta();
 
-        // constructor vacio
-        var aritmetica2 = new Aritmetica();
         System.out.println();
+
+        var aritmetica2 = new Aritmetica();
         aritmetica2.operando1 = 12;
-        aritmetica2.operando2 = 8;
+        aritmetica2.operando2 = 16;
         aritmetica2.sumar();
         aritmetica2.resta();
 
--- Clases/src/ArtimeticaInputs.java
+++ Clases/src/ArtimeticaInputs.java
@@ -1,19 +1,8 @@
 import java.util.Scanner;
 
-public class Aritmetica {
+public class ArtimeticaInputs {
     int operando1;
     int operando2;
-
-    // Constructor vacío
-    public Aritmetica(){
-    }
-
-    // Constructor con dos argumentos
-    public Aritmetica(int op1, int op2){
-        System.out.println("Ejecutando constructor");
-        operando1 = op1;
-        operando2 = op2;
-    }
 
     void sumar() {
         var resultado = operando1 + operando2;
@@ -25,19 +14,25 @@ public class Aritmetica {
         System.out.println("Resultado de la resta: " + resultado2);
     }
 
+    void input() {
+        Scanner consola = new Scanner(System.in);
+        System.out.print("Ingresa el valor para el operando1: ");
+        operando1 = consola.nextInt();
+        System.out.print("Ingresa el valor para el operando1: ");
+        operando2 = consola.nextInt();
+    }
 
     public static void main(String[] args) {
         System.out.println("*** Ejemplo Aritmetica ***");
-        var aritmetica1 = new Aritmetica(5, 7);
-        // aritmetica1.input();
+        var aritmetica1 = new ArtimeticaInputs();
+        aritmetica1.input();
         aritmetica1.sumar();
         aritmetica1.resta();
 
-        // constructor vacio
-        var aritmetica2 = new Aritmetica();
         System.out.println();
-        aritmetica2.operando1 = 12;
-        aritmetica2.operando2 = 8;
+
+        var aritmetica2 = new ArtimeticaInputs();
+        aritmetica2.input(); // -> Se parece a un ciclo, ya que te pide de nuevo valores, pero no lo es, ya que está llamando el método "input"
         aritmetica2.sumar();
         aritmetica2.resta();
 
