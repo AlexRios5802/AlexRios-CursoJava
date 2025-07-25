582de8668ca4a3abf05febda6ee530b17bf8f119
Alex Rios <al066668@uacam.mx>
2025-07-24 23:45:28 -0600

    Agregamos el tema de ENCAPSULAMIENTO e identificamos su importancia

--- Clases/src/aritmetica/Aritmetica.java
+++ Clases/src/aritmetica/Aritmetica.java
@@ -1,8 +1,8 @@
 package aritmetica; // <--- Esto se agregó justo de crear el paquete
 
 public class Aritmetica {
-    int operando1;
-    int operando2;
+    private int operando1; // <<= Al ponerlo en público podemos usar estas variables en mi PruebaAritmetica.java
+    private int operando2;
 
     // Constructor vacío
     public Aritmetica(){
@@ -24,6 +24,22 @@ public class Aritmetica {
     public void resta() {
         var resultado2 = operando1 - operando2;
         System.out.println("Resultado de la resta: " + resultado2);
+    }
+
+
+    public int getOperando1(){
+        return  this.operando1;
+    }
+    public void setOperando1(int operando1){
+        this.operando1 = operando1;
+    }
+
+
+    public int getOperando2(){
+        return this.operando2;
+    }
+    public void setOperando2(int operando2){
+        this.operando2 = operando2;
     }
 
 }
